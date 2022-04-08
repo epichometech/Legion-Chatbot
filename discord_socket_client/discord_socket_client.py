ee1a0ffd699ddd7094ebe09003f889cbe01d82f8
Jeremy-SohOvercomplicated <jeremy@sohovercomplicated.com>
2022-04-08 18:16:33 -0400

    adapt for kafka library vs confluent-kafka

--- discord_socket_client/discord_socket_client.py
+++ discord_socket_client/discord_socket_client.py
@@ -1,25 +1,29 @@
 import json, sys, socket, os
 import parsers.discord.discord_parse as discord_parse
-from confluent_kafka import Producer
+import redis
+
+from kafka import KafkaProducer, KafkaAdminClient
+from kafka.admin import NewTopic
 
 import discord
 from discord.ext.commands import Bot
 
-client = discord.Client()
+
 
 KAFKA_SERVER = os.getenv('KAFKA_SERVER',default='kafka')
 KAFKA_PORT = os.getenv('KAFKA_PORT',default='9092')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN',default=None)
+TOPICS= [
+  'discordMessagesIncoming'
+]
+REDIS_SERVER = os.getenv('REDIS_SERVER',default='redis')
+REDIS_PORT = os.getenv('REDIS_PORT',default=10)
 
-conf = {
-  'bootstrap.servers': f'{KAFKA_SERVER}:{KAFKA_PORT}',
-}
 
-producer = Producer(conf)
+client = discord.Client()
 
-def delivery_callback(err, msg):
-  if err:
-    sys.stderr.write(f'%% Message failed delivery: {err}\n')
+def on_errback(err):
+  sys.stderr.write(f'%% Message failed delivery: {err}\n')
 
 @client.event
 async def on_ready():
@@ -28,21 +32,29 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return
+        pass
+        #return
     try:
       parsedMessage = discord_parse.parse_discord_object(message)
-      '''This topic needs to be a configuration variable'''
-      producer.produce('discordMessagesIncoming',json.dumps(parsedMessage), callback=delivery_callback)
-    except BufferError:
-      sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
-        len(producer))
+      producer.send('discordMessagesIncoming',json.dumps(parsedMessage)).add_errback(on_errback)
     except Exception as e:
       raise e
     producer.flush()
     print(f'Message sent')
 
-if DISCORD_TOKEN:
-  client.run(DISCORD_TOKEN)
-else:
-  print('No discord token set. Set DISCORD_TOKEN and restart.')
-  sys.exit(1)
+if __name__ == "__main__":
+  if DISCORD_TOKEN:
+    producer = KafkaProducer(bootstrap_servers=[f'{KAFKA_SERVER}:{KAFKA_PORT}'])
+    admin = KafkaAdminClient(bootstrap_servers=[f'{KAFKA_SERVER}:{KAFKA_PORT}'])
+    for item in TOPICS:
+      try:
+        topic = NewTopic(name=item, num_partitions=1, replication_factor=1)
+        admin.create_topics([topic])
+      except Exception:
+        print(f'Topic {item} is already created')
+    admin.close()
+    admin = None
+    client.run(DISCORD_TOKEN)
+  else:
+    print('No discord token set. Set DISCORD_TOKEN and restart.')
+    sys.exit(1)
