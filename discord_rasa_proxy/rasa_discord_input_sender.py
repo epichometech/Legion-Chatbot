becddcb204ef79501c7ea9a32961a2d43c0dc43e
Jeremy-SohOvercomplicated <jeremy@sohovercomplicated.com>
2022-04-08 22:01:38 -0400

    PYTHONUNBUFFERED

--- discord_rasa_proxy/rasa_discord_input_sender.py
+++ discord_rasa_proxy/rasa_discord_input_sender.py
@@ -1,17 +1,14 @@
-import json, sys, time
+import json, sys, time, signal
 from json.decoder import JSONDecodeError
-from confluent_kafka import Consumer, KafkaException
-
-conf = {
-  'bootstrap.servers': "kafka:9092",
-  'group.id': 'rasa',
-  'session.timeout.ms': 6000,
-  'auto.offset.reset': 'earliest'
-}
+from kafka import KafkaConsumer
 
 class KafkaPush2Rasa:
-  def __init__(self, conf):
-    self.consumer = Consumer(conf)
+  def __init__(self, conf=None):
+    self.consumer = KafkaConsumer(
+      'discordMessagesIncoming',
+      bootstrap_servers = ["kafka:9092"],
+      group_id = "rasa"
+    )
 
   def print_assignment(self, consumer, partitions):
     print(f'Assignment: {partitions}')
@@ -19,29 +16,30 @@ class KafkaPush2Rasa:
   def delivery_callback(self, err, msg):
     if err:
       sys.stderr.write('%% Message failed delivery: %s\n' % err)
+  
+  def graceful_exit(self, *args, **kwargs):
+    self.is_shutting_down = True
 
   def run(self):
-    self.consumer.subscribe(['discordMessagesIncoming'], on_assign=self.print_assignment)
-
     try:
-      while True:
-        msg = self.consumer.poll(timeout=1.0)
-        if msg is None:
-          continue
-        if msg.error():
-          raise KafkaException(msg.error())
-        try:
-          message = json.loads(str(msg.value().decode("utf-8")))
-          print(f'Message received')
-        except JSONDecodeError as e:
-          print('Failed to decode json value in kafka message')
-          print(e)
-          print(str(msg.value().decode("utf-8")))
+        for msg in self.consumer:
+          try:
+            message = json.loads(str(msg.value().decode("utf-8")))
+            print(f'Message received')
+          except JSONDecodeError as e:
+            print('Failed to decode json value in kafka message')
+            print(e)
+            print(str(msg.value().decode("utf-8")))
     except KeyboardInterrupt:
       print('%% Aborted by user\n')
     except Exception as e:
       raise e
 
+def shutdown(*args, **kwargs):
+  pusher.graceful_exit()
+
 if __name__ == "__main__":
-  pusher = KafkaPush2Rasa(conf)
+  pusher = KafkaPush2Rasa()
+  signal.signal(signal.SIGINT, shutdown)
+  signal.signal(signal.SIGTERM, shutdown)
   pusher.run()
